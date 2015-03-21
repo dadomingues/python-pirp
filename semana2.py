ac3a2022e9e12fa5a04f2a020d012c37458d2e36
Daniel Araujo Domingues <dadomingues@users.noreply.github.com>
2015-03-21 19:12:54 -0300

    Update semana2.py

--- semana2.py
+++ semana2.py
@@ -38,3 +38,46 @@ def x_indices2(N):
     f[i,i] = True
     f[i,-i-1] = True
     return f
+    
+def x_idespelho(N):
+    # cria uma matriz em que tudo é falso com a
+    # exceção da posição onde os ídices são iguais;
+    # depois realiza adição com seu próprio espelho.
+    f = np.identity(N,'bool')
+    return f | f[:,::-1]
+
+# crop
+
+def crop_vh(f):
+    # verifica se as linhas que possuem algum valor maior que zero
+    # verifica se as coluns que possuem algum valor maior que zero
+    # gera uma saída com o pedaço em que existe valores maiores que zero.
+    import numpy as np
+    maxR = f.max(1)
+    (iR,) = maxR.nonzero()
+    f = f[iR[0]:iR[-1]+1] # crop na altura / linhas
+    maxC = f.max(0)
+    (iC,) = maxC.nonzero()
+    return  f[:, iC[0]:iC[-1]+1] # crop na largura / colunas
+    
+def crop_limites(f):
+    # busca os pontos X e Y iniciais e finais
+    # da área a ser cortada e retorna o conteúdo.
+    r = f.max(1)  # projeção horizontal
+    c = f.max(0)  # projeção vertical
+    ri = r.nonzero()[0]  # índices de pixels com valores não zeros
+    ci = c.nonzero()[0]
+    rmin = ri[0]         # primeiro índice de pixel com valor não zero
+    rmax = ri[-1] + 1    # último índice de pixel com valor não zero
+    cmin = ci[0]
+    cmax = ci[-1] + 1
+    return f[rmin:rmax, cmin:cmax]
+    
+def crop_indices(f):
+    # pouco eficiente
+    rc = f.nonzero()
+    rmin = rc[0].min()
+    rmax = rc[0].max()+1
+    cmin = rc[1].min()
+    cmax = rc[1].max()+1
+    return f[rmin:rmax, cmin:cmax]
