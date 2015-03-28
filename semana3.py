73a48830f57cf8f2ad3aae67f80fcadf0f924f16
Daniel Araujo Domingues <dadomingues@users.noreply.github.com>
2015-03-28 18:40:29 -0300

    Update semana3.py

--- semana3.py
+++ semana3.py
@@ -29,5 +29,27 @@ L = 15
 Tw = TWL(L,W)
 adshow(ia.iaplot(Tw,ylabel='Output intensity',xlabel='Input intensity'),'Transformada de intensidade W=%d L=%d' % (W,L)
 
+# exercício de reescrita do affine
 
+# melhor solução (performance) por yugo4k a partir de barbara_reis
+def gg_affine4(f, T):
+  import numpy as np
+  H, W = domain = f.shape
+  n = H * W
+  y1,x1 = np.indices(domain)
+
+  yx1 = np.array([y1.ravel(),
+                  x1.ravel(),
+                  np.ones(n)])
+  yx_float = np.dot(np.linalg.inv(T), yx1)
+  yy = np.rint(yx_float[0]).astype(int)
+  xx = np.rint(yx_float[1]).astype(int)
+
+  g = np.zeros(n, dtype=np.uint8)
+  validIdxs = (0 <= yy) & (yy < H) & (0 <= xx) & (xx < W)
+  g[validIdxs] = f[yy[validIdxs], xx[validIdxs]]
+
+  return g.reshape(domain)
+
+list_gg_affine.append(gg_affine4)
 
